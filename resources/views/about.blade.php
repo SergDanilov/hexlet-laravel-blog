15c2b959432778d08fd3d1de06b45d37d147a593
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-07-19 14:34:01 +0300

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,10 @@
-<html>
-    <head>
-        <title>Hexlet Blog - @yield('title')</title>
-        @include('shared.metatags')
-    </head>
-    <body>
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
