f9e3d101464f22a33af17815b1784827292a0ef5
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-07-23 12:37:07 +0300

    Update 2024_07_23_103154_create_articles_table.php

--- database/migrations/2024_07_23_103154_create_articles_table.php
+++ database/migrations/2024_07_23_103154_create_articles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->name();
+            $table->body();
             $table->timestamps();
         });
     }
