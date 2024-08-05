b50c9c20e796ddb257e724e1f2cc45f01c9bd7ec
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 13:21:31 +0300

    Update create.blade.php

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,1 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Создать статью</h2>
+    {{ html()->modelForm($article, 'POST', route('articles.store'))->open() }}
+        {{  html()->label('Имя', 'name') }}
+        {{  html()->input('text', 'name') }}
+        {{  html()->label('Содержание', 'body') }}
+        {{  html()->textarea('body') }}
+        {{ html()->submit('Save') }}
+    {{ html()->closeModelForm() }}
+@endsection
 
