ef24b1de1120f709a1d1d039c2758b9d9d6912d9
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 12:50:14 +0300

    Update ArticleCategoryController.php

--- app/Http/Controllers/ArticleCategoryController.php
+++ app/Http/Controllers/ArticleCategoryController.php
@@ -11,5 +11,33 @@ class ArticleCategoryController extends Controller
         $article_categories = ArticleCategory::paginate(10);
         // Статьи передаются в шаблон
         return view('article_category.index', compact('article_categories'));
-    	}
+    }
+    
+    public function create()
+    {
+        // Передаем в шаблон вновь созданный объект. Он нужен для вывода формы
+        $article_category = new ArticleCategory();
+        return view('article.create', compact('article_category'));
+    }
+    
+    public function store(Request $request)
+    {
+        // Проверка введенных данных
+        // Если будут ошибки, то возникнет исключение
+        // Иначе возвращаются данные формы
+        $data = $this->validate($request, [
+            'name_category' => 'required|unique:article_categories',
+            'body' => 'required|max:250',
+        ]);
+
+        $article_category = new ArticleCategory();
+        // Заполнение статьи данными из формы
+        $article_category->fill($data);
+        // При ошибках сохранения возникнет исключение
+        $article_category->save();
+
+        // Редирект на указанный маршрут
+        return redirect()
+            ->route('$article_category.index');
+    }
 }
