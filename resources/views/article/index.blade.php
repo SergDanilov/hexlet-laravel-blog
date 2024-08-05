5db0c91320c80269dd1b8cf98fa1ea8e9db8ae56
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 13:41:57 +0300

    Update index.blade.php

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+    <h2>найти статью</h2>    
+    {{  html()->form('GET', route('articles.index'))->open() }}
+        {{ html()->text('q', $q) }}
+        {{ html()->submit('Найти') }}
+    {{ html()->form()->close() }}
     <h1>Список статей</h1>
     @foreach ($articles as $article)
         <h2>{{$article->name}}</h2>
