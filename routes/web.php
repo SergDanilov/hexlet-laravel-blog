35e780c5bb84c5b236578b8c8d3f8fb203173a6e
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 11:03:40 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', ['PageController::class', 'about'])
+Route::get('about', [PageController::class, 'about'])
 	->name('about');
 
 Route::get('articles/create', 'ArticleController@create')
