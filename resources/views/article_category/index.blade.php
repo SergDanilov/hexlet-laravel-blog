014b1e8521c750bfd67d2c3f76f046fcafed2bfe
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 13:09:19 +0300

    Update index.blade.php

--- resources/views/article_category/index.blade.php
+++ resources/views/article_category/index.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Список категорий</h1>
-    @foreach ($article_categories as $category)
-        <h3>{{ $category->name }}</h3>
+    <h1>{{$article_сategory->name}}</h1>
+    <div>{{$article_сategory->body}}</div>
 
-    @endforeach
+
+    @if ($article_сategory->articles->isEmpty())
+        I don't have any records!
+    @else
+        <ol>
+        @foreach ($article_сategory->articles as $article)
+            <li>
+            <a href="{{ route('articles.show', $article) }}">{{$article->name}}</a>
+            </li>
+        @endforeach
+        </ol>
+    @endif
+
 @endsection
-
