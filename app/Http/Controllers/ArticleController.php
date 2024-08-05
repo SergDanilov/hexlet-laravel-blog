9e2ccb82a6d81cb31c0b1fc5fdb1443621d08816
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 13:43:00 +0300

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,13 +7,12 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $articles = Article::paginate();
-
-        // Статьи передаются в шаблон
-        // compact('articles') => [ 'articles' => $articles ]
-        return view('article.index', compact('articles'));
+        $q = $request->input('q');
+        // Like has huge impact on the performance. Use them carefully. Learn indexes and full text search.
+        $articles = $q ? Article::where('name', 'ilike', "%{$q}%")->paginate() : Article::paginate();
+        return view('article.index', compact('articles', 'q'));
     }
 
     public function create()
