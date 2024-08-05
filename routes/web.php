df8e05aad0bf4f61b122a1e0b50f880fb8179528
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-08-05 10:56:44 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
